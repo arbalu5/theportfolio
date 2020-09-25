996ec85a435cd47f7dd4fd04e0c052cfab998ba4
Lucica Arba <arba.lucica@gmail.com>
2020-09-25 10:30:14 +0200

    add jokes section

---
+++ web/src/components/Jokes.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+class Jokes extends Component {
+  state = { quote: {}, jokes: [] };
+
+  componentDidMount() {
+    fetch("https://api.quotable.io/random")
+      .then((response) => response.json())
+      .then((json) => this.setState({ quote: json }));
+  }
+
+  fetchJokes = () => {
+    fetch("https://official-joke-api.appspot.com/random_ten")
+      .then((response) => response.json())
+      .then((json) => this.setState({ jokes: json }));
+  };
+
+  render() {
+    const { content, author } = this.state.quote;
+
+    return (
+      <div>
+        <h3>Famous Quotes:</h3>
+        <p>
+          <em>"{content}" - {author}</em>
+        </p>
+        <hr />
+        <h3>Want some jokes?</h3>
+        <button onClick={this.fetchJokes}>Get 10 Jokes!</button>
+        {this.state.jokes.map((joke) => {
+          const { id, setup, punchline } = joke;
+          return (
+            <p key={id}>
+              <em>{setup} - {punchline}</em>
+            </p>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default Jokes;
