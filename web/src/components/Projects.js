1fa1efee84161176eafeadd4bc8235a45dbdb698
Lucica Arba <arba.lucica@gmail.com>
2020-09-25 14:58:50 +0200

    update projects

--- web/src/components/Projects.js
+++ web/src/components/Projects.js
@@ -9,7 +9,7 @@ const Project = (props) => {
       <h3>{title}</h3>
       <img src={image} alt="profile" style={{ width: 200, height: 120 }} />
       <p>{description}</p>
-      <a href={link}>Check in Github</a>
+      <a href={link}>View in Github</a>
     </div>
   );
 };
